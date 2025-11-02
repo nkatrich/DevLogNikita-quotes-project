28d4d83eb20dd007318e6087be7f1f46efea1b48
Nikita <nikitakatrich.kzn@gmail.com>
2025-11-02 17:08:36 +0300

    added JS code into index.html

---
+++ scripts/main.js
@@ -0,0 +1,40 @@
+// Vars
+
+const burgerMenu = document.querySelector('.burger-menu');
+const navHeaderNavPhone = document.querySelector('.nav-header-nav-phone');
+
+// Burger-menu
+
+function toggleBurgerMenu() {
+    navHeaderNavPhone.classList.toggle('open');
+}
+
+burgerMenu.addEventListener('click', toggleBurgerMenu);
+
+// Smooth scroll
+
+document.addEventListener('DOMContentLoaded', () => {
+    window.addEventListener('wheel', (e) => {
+        e.preventDefault();
+
+        const cordScroll = 200;
+
+        let posSiteScroll = window.scrollY
+
+        if (e.deltaY > 0) {
+            posSiteScroll += cordScroll;
+        }
+        else {
+            posSiteScroll -= cordScroll;
+            if (posSiteScroll < 0) {
+                posSiteScroll = 0;
+            }
+        }
+
+        window.scrollTo({
+            top: posSiteScroll,
+            behavior: 'smooth'
+        })
+
+    }, {passive: false});
+})
